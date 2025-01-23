e427c948848175a9d59aeffb934c18f417925361
pablif <nhz01530@educastur.es>
2025-01-23 19:08:47 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './Header';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a heading', () => {
+    render(<Header />);
+    const headingElement = screen.getByRole('heading');
+    expect(headingElement).toBeInTheDocument();
+});
+
+test('handles clicks on the navigation link', () => {
+    render(<Header />);
+    const navLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(navLink);
+    // Aquí puedes verificar si se ha disparado algún evento o si se ha navegado a otra página
 });
