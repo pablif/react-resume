3cb08b4c89e378d5214fa7ce75db45810edad32f
pablif <nhz01530@educastur.es>
2025-01-23 20:12:41 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import from './App';
 import Header from './Header';
 import userEvent from '@testing-library/user-event';
 
