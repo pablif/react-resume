5c66ce8d0b79aa921e87117472d92d279d82a49f
pablif <nhz01530@educastur.es>
2025-01-25 18:18:22 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import from './App';
-import Header from './Header';
-import userEvent from '@testing-library/user-event';
+import App from './App';
+import data from "./data.json";
 
-test('renders a heading', () => {
-    render(<Header />);
-    const headingElement = screen.getByRole('heading');
-    expect(headingElement).toBeInTheDocument();
+test('aplicar tema por defecto', () => {
+  // Mock window.matchMedia
+  window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+      matches: query === '(prefers-color-scheme: dark)',
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  });
+
+  render(<App />);
+
+  const htmlElement = document.documentElement;
+  const appliedTheme = htmlElement.getAttribute('data-theme');
+
+  expect(['light', 'dark']).toContain(appliedTheme);
 });
 
-test('handles clicks on the navigation link', () => {
-    render(<Header />);
-    const navLink = screen.getByRole('link', { name: 'About' });
-    userEvent.click(navLink);
-    // Aquí puedes verificar si se ha disparado algún evento o si se ha navegado a otra página
+test('renderiza toda la informacion', () => {
+  render(<App />);
+
+  // Verificar que los componentes clave están presentes
+  expect(screen.getByText(data.info.name)).toBeInTheDocument(); // Header
+  expect(screen.getByText(data.experience[0].company)).toBeInTheDocument(); // ExperienceSection
+  expect(screen.getByText(data.skills[0].name)).toBeInTheDocument(); // SkillsSection
+  expect(screen.getByText(data.education[0].degree)).toBeInTheDocument(); // EducationSection
 });
